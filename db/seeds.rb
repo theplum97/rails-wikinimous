e4956b28567a4d56f99ea85974b0a2ed2d2534ab
Marvin Pflaume <pflaume.marvin@gmail.com>
2020-11-17 16:12:34 +0100

    added everything

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  article_new = Article.new(title: Faker::FunnyName.name, content: Faker::Movie.quote)
+  article_new.save
+end
